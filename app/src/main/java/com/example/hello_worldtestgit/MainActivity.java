68ae5ac1fd776f9123d3dbf1c33a8469d9101fec
Emanuele <emanuele.drago@outlook.it>
2019-03-08 15:33:26 +0100

    Esercitazione 2 aggiunta di un campo di testo e un bottone

--- app/src/main/java/com/example/hello_worldtestgit/MainActivity.java
+++ app/src/main/java/com/example/hello_worldtestgit/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.hello_worldtestgit;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //test Sync con github
-        //modifiche
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage (View view){
+        EditText message_edittext = (EditText) findViewById(R.id.editText);
+        String message = message_edittext.getText().toString();
+        Uri uri = Uri.parse("smsto:0800000123");
+        Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+        it.putExtra("sms_body", message); startActivity(it);
     }
 }
