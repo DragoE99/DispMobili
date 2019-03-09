e4765ec2346cbd7cdf700356536a225e6630fd5f
Drago <emanuele.drago@outlook.it>
2019-03-09 16:53:26 +0100

    primo commit

---
+++ app/src/main/java/com/example/testactivity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.testactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //primo commento
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
